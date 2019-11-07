dc76022f0696b75a4fca7fa1fb2b430da76833ea
SanZamoyski <san.zamoyski@wp.pl>
2019-11-07 16:53:19 +0100

    Create H-Grbl.ino

---
+++ H-Grbl.ino
@@ -0,0 +1,32 @@
+/* ~ GRBL with L293d (H-Bridge) and DC Motor Pen mechanism ~
+     ╦═╗╔╗ ╦╔═
+By-- ╠╦╝╠╩╗╠╩╗ - 2016 Rakshith BK
+     ╩╚═╚═╝╩ ╩
+*
+* No licences no copyrights, well may be MIT.
+* Use modify distribute and do whatever you want
+* And even though this code will not launch any Nukes,
+* I'm not responsible for any destruction caused by it.!
+*
+* X-axis stepper --> 8, 9, 10, 11
+* Y-axis stepper --> 4, 5, 6, 7
+* DC motor pin for Pen --> 12 for Pen Up - M4
+* for Pen Down - M3
+*
+*
+* For Details and working --
+*/
+
+/*
+* Moved to v1.1 by San Zamoyski 11-2019
+* */
+
+#include "grbl.h"
+
+void setup() {
+
+}
+
+void loop() {
+
+}
