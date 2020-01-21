9d969e064bf20ef8f13c99ae6ee075480cfd0e9a
SanZamoyski <san.zamoyski@wp.pl>
2020-01-21 10:29:13 +0100

    Pinouts info update

--- H-Grbl.ino
+++ H-Grbl.ino
@@ -6,19 +6,18 @@ By-- ╠╦╝╠╩╗╠╩╗ - 2016 Rakshith BK
 * No licences no copyrights, well may be MIT.
 * Use modify distribute and do whatever you want
 * And even though this code will not launch any Nukes,
-* I'm not responsible for any destruction caused by it.!
-*
-* X-axis stepper --> 8, 9, 10, 11
-* Y-axis stepper --> 4, 5, 6, 7
-* DC motor pin for Pen --> 12 for Pen Up - M4
-* for Pen Down - M3
-*
+* I'm not responsible for any destruction caused by it.!*
 *
 * For Details and working --
 */
 
 /*
-* Moved to v1.1 by San Zamoyski 11-2019
+* Moved to v1.1 by San Zamoyski 2019-2020
+*
+* X-axis stepper --> 2, 3, 4, 5
+* Y-axis stepper --> 6, 7, 8, *12*
+* X, Y Limit --> 9, 10
+* Laser --> 11
 * */
 
 #include "grbl.h"
